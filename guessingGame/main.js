3598bb1077ba86952119dd72c60965f3abf8f45a
Tare Evans <tamaratarelayefa@gmail.com>
2025-06-04 17:23:48 +0100

    adding files

---
+++ guessingGame/main.js
@@ -0,0 +1,62 @@
+let landing = document.getElementById('landing');
+let startBtn = document.getElementById('startBtn');
+let gamebox = document.getElementById('gamebox');
+
+startBtn.addEventListener('click', () => {
+  
+  if (gamebox.style.display = 'none') {
+    gamebox.style.display = 'block';
+    landing.style.display = 'none';
+  }
+});
+
+let check = document.getElementById('check');
+check.addEventListener('click', checker);
+
+let inputBox = document.querySelector('input[name="box"]');
+inputBox.addEventListener('focus', () => {
+  message.style.display = 'none';
+});
+
+
+let myLabel = document.getElementById('myLabel');
+let message = document.getElementById('message')
+
+let counter = 0;
+
+function checker(val) {
+  counter++;
+  let inputBoxValue = document.querySelector('input[name="box"]').value;
+  let generatedNum = Math.floor(Math.random()*50) +1;
+  
+  if (Number(inputBoxValue) === generatedNum) {
+    message.style.display = 'block';
+    message.textContent = 'Correct! Good Job!';
+    message.style.color = 'green';
+  } else {
+    message.style.display = 'block'
+    message.textContent = 'Incorrect, Try Again'
+    message.style.color = 'red';
+  }
+  
+  
+  inputBox.value = '';
+  recoder(counter, inputBoxValue, generatedNum);
+}
+
+function recoder(num, userVal, genVal) {
+  let tab = document.getElementById('tab');
+  let newEle = document.createElement('tr');
+  let newEle1 = document.createElement('td');
+  let newEle2 = document.createElement('td');
+  
+  
+  newEle1.textContent = `${num}) `
+  newEle2.textContent = `You Entered: ${userVal} | Correct Answer: ${genVal}`
+  
+  tab.appendChild(newEle);
+  newEle.appendChild(newEle1)
+  newEle.appendChild(newEle2);
+  
+  
+}
